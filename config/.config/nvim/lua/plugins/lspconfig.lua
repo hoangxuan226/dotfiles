44e735866155693cf533123e4ff632a7aafa96f6
hoangxuan226 <phamxuanhoang99@gmail.com>
2025-12-17 01:17:44 +0700

    feat(nvim): replace omnisharp with roslyn.nvim and configure LSP
    - Removed `lazyvim.plugins.extras.lang.dotnet` from `lazyvim.json`.
    - Added `roslyn.nvim` plugin with default configuration.
    - Disabled `omnisharp` LSP in `nvim-lspconfig`.
    - Configured `mason.nvim` to use custom registries and ensure `roslyn`
    is installed.

---
+++ config/.config/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        omnisharp = { enabled = false },
+      },
+    },
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      registries = {
+        "github:mason-org/mason-registry",
+        "github:Crashdummyy/mason-registry",
+      },
+      ensure_installed = {
+        "roslyn",
+      },
+    },
+  },
+  "seblyng/roslyn.nvim",
+  ---@module 'roslyn.config'
+  ---@type RoslynNvimConfig
+  opts = {
+    -- your configuration comes here; leave empty for default settings
+  },
+}
